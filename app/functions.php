2944cf0aaa565680eb0ce5aa587979a0c4c10abc
Prince Asiedu <princekofasiedu@gmail.com>
2023-10-06 08:29:25 +0000

    Added get_data function

--- app/functions.php
+++ app/functions.php
@@ -1,5 +1,18 @@
 <?php
 
+function get_data()
+{
+    $fname = CONFIG['data_file'];
+    $json_content = ' ';
+
+    if(!file_exists($fname)) {
+        file_put_contents($fname, '');
+    }else {
+        $json_content = file_get_contents($fname); 
+    }
+
+    return $json_content;
+}
 function view($name, $model)
 {
     require("views/layouts.view.php");
